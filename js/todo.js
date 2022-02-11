09e46b88fcbbd0963a70dcfe97cb816e930bb052
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-02-12 00:02:17 +0600

    all done

--- js/todo.js
+++ js/todo.js
@@ -14,10 +14,24 @@ document.getElementById('add-btn').addEventListener('click', function () {
 
         tabaleContainer.innerHTML = `<th>${count}</th>
         <td>${takeText}</td>
-        <td><button type="button" class="btn btn-danger">Delete</button>
-        <button type="button" class="btn btn-success">Done</button></td>`;
+        <td><button type="button" class="btn btn-danger deleteItem">Delete</button>
+        <button type="button" class="btn btn-success doneItem">Done</button></td>`;
 
         mainContainer.appendChild(tabaleContainer);
+    }
+
+    const itemDelete = document.getElementsByClassName('deleteItem');
+    for (const item of itemDelete) {
+        item.addEventListener('click', function (event) {
+            event.target.parentNode.parentNode.style.display = 'none';
+        })
+    }
+
+    const itemDone = document.getElementsByClassName('doneItem');
+    for (const item of itemDone) {
+        item.addEventListener('click', function (event) {
+            event.target.parentNode.parentNode.style.textDecoration = 'line-through';
+        })
     }
 })
 
